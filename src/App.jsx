2512d229c3b443aede7945eff581825bdd606208
forassignment1221 <forassignment1221@gmail.com>
2026-05-21 18:39:07 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-
+import { useState, useEffect, Suspense } from 'react';
 
 import './App.css'
 import Users from './components/Users'
 
 function App() {
  
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/users")
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+  console.log("Users in app: ", users)
 
   return (
     <>
       <h1>Simple Crud</h1>
-      <Users></Users>
+      <Suspense fallback={<p>Loading....</p>}>
+      <Users users={users} ></Users>
+      </Suspense>
       
     </>
   )
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,39 +1,72 @@
+import { useEffect, useState } from "react";
 
+const Users = ({ users }) => {
+  const [allUsers, setAllUsers] = useState([]);
 
-const Users = () => {
-    const handleAddUser=e=>{
-        e.preventDefault();
-        const name=e.target.name.value;
-        const email=e.target.email.value;
-        console.log(name,email);
-        const newUser={name,email};
+  useEffect(() => {
+    setAllUsers(users);
+  }, [users]);
 
-        //save this user data to db (via server)
-        fetch("http://localhost:3000/users", {
-            method:"POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newUser)
-            
-        })
-        .then(res=>res.json())
-        .then(data=>console.log("Post successfull with data:", data))
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    console.log(name, email);
+    const newUser = { name, email };
 
+    //save this user data to db (via server)
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Post successful with data:", data);
+        if (data.insertedId) {
+          alert("User is added with id", data.insertedId);
+          setAllUsers([...allUsers, newUser]);
+        }
+      });
+  };
+  const handleDeleteButton=(userId)=>{
+    fetch('http://localhost:3000/delete/:2').
 
-
-    }
-    return (
-        <div>
-            <form onSubmit={handleAddUser}>
-                <input type="text" name="name" />
-                <br />
-                <input type="email" name="email" />
-                <br />
-                <input type="submit" value="Add User" />
-            </form>
-        </div>
-    );
+  }
+  return (
+    <div>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" />
+        <br />
+        <input type="email" name="email" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+      <br />
+      <h1>Users</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {allUsers.map((user, index) => {
+            return (
+              <tr key={index}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td><button onClick={()=>handleDeleteButton(user.id)}>Delete</button></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Users;
